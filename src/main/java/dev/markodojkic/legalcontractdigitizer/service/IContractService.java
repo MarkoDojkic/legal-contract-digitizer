120e9677fe6984b62744c06497f25bd2f928973f
Марко Дојкић <marko.dojkic@gmail.com>
2025-06-20 19:41:52 +0200

    Refactored AI service use to direct WebClient request sending, since Spring AI had issues with parsing responses; Corrected contract deployment logic; Code refactoring of records and pom cleanup

--- src/main/java/dev/markodojkic/legalcontractdigitizer/service/IContractService.java
+++ src/main/java/dev/markodojkic/legalcontractdigitizer/service/IContractService.java
@@ -2,12 +2,14 @@ package dev.markodojkic.legalcontractdigitizer.service;
 
 import dev.markodojkic.legalcontractdigitizer.dto.DeploymentStatusResponseDTO;
 
+import java.math.BigInteger;
 import java.util.List;
 
 public interface IContractService {
 	String saveUploadedContract(String contractText);
+	DeploymentStatusResponseDTO getContractStatus(String contractId);
 	List<String> extractClauses(String contractText);
 	String generateSolidity(String contractId);
 	String deployContractWithParams(String contractId, List<Object> constructorParams);
-	DeploymentStatusResponseDTO getContractStatus(String contractId);
+	BigInteger estimateGasForDeployment(String contractId, List<Object> constructorParams);
 }
